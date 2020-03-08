e7b67fdf75811daf3bd7b4eaccc44b5ae3af61d2
Magnus <msolvaag@gmail.com>
2020-03-08 15:06:21 +0100

    tabs

--- heatit-z-trm2fx.groovy
+++ heatit-z-trm2fx.groovy
@@ -3,21 +3,21 @@
  */
 metadata {
 	definition (name: "Heatit Z-TRM2fx", namespace: "ms", author: "Magnus Solvåg", ocfDeviceType: "oic.d.thermostat", cstHandler: true) {
-        capability "Actuator"
-        capability "Temperature Measurement"
-        capability "Thermostat Mode"
-        capability "Thermostat Heating Setpoint"
-        capability "Thermostat Operating State"
-        capability "Energy Meter"
-        capability "Power Meter"
-        capability "Refresh"
-        capability "Sensor"
-        capability "Health Check"
+		capability "Actuator"
+		capability "Temperature Measurement"
+		capability "Thermostat Mode"
+		capability "Thermostat Heating Setpoint"
+		capability "Thermostat Operating State"
+		capability "Energy Meter"
+		capability "Power Meter"
+		capability "Refresh"
+		capability "Sensor"
+		capability "Health Check"
 
 		command "switchMode"
 		command "lowerHeatingSetpoint"
 		command "raiseHeatingSetpoint"
-        command "setup"
+		command "setup"
 
 		fingerprint mfr: "019B", prod: "0003", model: "0202", deviceJoinName: "HeatIt Z-TRM2fx"
 	}
@@ -31,14 +31,14 @@ metadata {
 			tileAttribute("device.temperature", key: "PRIMARY_CONTROL") {
 				attributeState("temp", label:'${currentValue}°', defaultState: true)
 			}
-            tileAttribute("device.thermostatOperatingState", key: "OPERATING_STATE") {
-                attributeState("idle", backgroundColor:"#00A0DC", label:'${currentValue}')
-                attributeState("heating", backgroundColor:"#e86d13", label:'${currentValue}')
-            }
+			tileAttribute("device.thermostatOperatingState", key: "OPERATING_STATE") {
+				attributeState("idle", backgroundColor:"#00A0DC", label:'${currentValue}')
+				attributeState("heating", backgroundColor:"#e86d13", label:'${currentValue}')
+			}
 		}
 		valueTile("operatingState", "device.thermostatOperatingState", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
 			state "heating", label:'On', backgroundColor: "#e86d13"
-            state "idle", label:'Off', backgroundColor: "#00A0DC", defaultState: true
+			state "idle", label:'Off', backgroundColor: "#00A0DC", defaultState: true
 		}
 		standardTile("mode", "device.thermostatMode", width:2, height:1, inactiveLabel: false, decoration: "flat") {
 			state "heat", action:"switchMode", nextState:"...", label: "Comfort"
@@ -52,13 +52,13 @@ metadata {
 		valueTile("power", "device.power", width: 3, height: 1, inactiveLabel: false, decoration: "flat") {
 			state "default", label:'${currentValue} W'
 		}
-        valueTile("energy", "device.energy", width: 3, height: 1, inactiveLabel: false, decoration: "flat") {
+		valueTile("energy", "device.energy", width: 3, height: 1, inactiveLabel: false, decoration: "flat") {
 			state "default", label:'${currentValue} kWh'
 		}
-       	standardTile("refresh", "command.refresh", width:2, height:1, inactiveLabel: false, decoration: "flat") {
+	   	standardTile("refresh", "command.refresh", width:2, height:1, inactiveLabel: false, decoration: "flat") {
 			state "default", action:"refresh", icon:"st.secondary.refresh"
 		}
-        standardTile("setup", "command.setup", width:2, height:1, inactiveLabel: false, decoration: "flat") {
+		standardTile("setup", "command.setup", width:2, height:1, inactiveLabel: false, decoration: "flat") {
 			state "default", action:"setup", label:"reset"
 		}
 		main "main"
@@ -103,27 +103,27 @@ private getMeterMetrics(){[
 
 private getOperatingStates(){[
 	(physicalgraph.zwave.commands.thermostatoperatingstatev1.ThermostatOperatingStateReport.OPERATING_STATE_IDLE) : "idle",
-    (physicalgraph.zwave.commands.thermostatoperatingstatev1.ThermostatOperatingStateReport.OPERATING_STATE_HEATING): "heating",
-    (physicalgraph.zwave.commands.thermostatoperatingstatev1.ThermostatOperatingStateReport.OPERATING_STATE_PENDING_HEAT): "pending"
+	(physicalgraph.zwave.commands.thermostatoperatingstatev1.ThermostatOperatingStateReport.OPERATING_STATE_HEATING): "heating",
+	(physicalgraph.zwave.commands.thermostatoperatingstatev1.ThermostatOperatingStateReport.OPERATING_STATE_PENDING_HEAT): "pending"
 ]}
 
 private getOperatingModes(){[
 	(physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport.MODE_OFF): "off",
-    (physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport.MODE_HEAT): "heat",
-    (physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport.MODE_ENERGY_SAVE_HEAT): "eco"
+	(physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport.MODE_HEAT): "heat",
+	(physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport.MODE_ENERGY_SAVE_HEAT): "eco"
 ]}
 
 private getConstants(){[
 	"checkInterval": 32 * 60,
-    "heatingModes": [1, 11],
-    "scale": [(0): "C", (1): "F"],
-    "metrics": ["energy": 0, "power": 2],
-    "channels": ["floorSensor": 3, "switch": 4]
+	"heatingModes": [1, 11],
+	"scale": [(0): "C", (1): "F"],
+	"metrics": ["energy": 0, "power": 2],
+	"channels": ["floorSensor": 3, "switch": 4]
 ]}
 
 private getConfigParams(){[
 	(19): [name: "tempReportInterval", size: 2, default: [0,0], unit: "seconds"],
-    (21): [name: "meterReportInterval", size: 2, default: [0,0], unit: "seconds"]
+	(21): [name: "meterReportInterval", size: 2, default: [0,0], unit: "seconds"]
 ]}
 
 private getCurrentMode(){ device.currentValue("thermostatMode") }
@@ -150,12 +150,12 @@ def setup() {
 	unschedule()
 	runEvery5Minutes("query")
 	runIn(1, "query", [overwrite: true])
-    
-    sendHubCommand([
-    	configParams.collect{param -> zwave.configurationV2.configurationSet(
-        	parameterNumber: param.key, size: param.value.size, configurationValue: param.value.default)},
-        configParams.collect{param -> zwave.configurationV2.configurationGet(parameterNumber: param.key)}
-    ].flatten())
+	
+	sendHubCommand([
+		configParams.collect{param -> zwave.configurationV2.configurationSet(
+			parameterNumber: param.key, size: param.value.size, configurationValue: param.value.default)},
+		configParams.collect{param -> zwave.configurationV2.configurationGet(parameterNumber: param.key)}
+	].flatten())
 }
 
 def query() {
@@ -164,7 +164,7 @@ def query() {
 
 	sendHubCommand([
 		zwave.thermostatModeV2.thermostatModeGet(),
-        zwave.thermostatSetpointV1.thermostatSetpointGet(setpointType: type),
+		zwave.thermostatSetpointV1.thermostatSetpointGet(setpointType: type),
 		toChannel(constants.channels.floorSensor, zwave.sensorMultilevelV5.sensorMultilevelGet()),
 		toChannel(constants.channels.switch, zwave.switchBinaryV1.switchBinaryGet())
 	])
@@ -173,20 +173,20 @@ def query() {
 def queryMetrics() {
 	log.info "queryMetrics"
 	sendHubCommand([
-        zwave.meterV3.meterGet(scale: constants.metrics.energy),
-        zwave.meterV3.meterGet(scale: constants.metrics.power)
+		zwave.meterV3.meterGet(scale: constants.metrics.energy),
+		zwave.meterV3.meterGet(scale: constants.metrics.power)
 	])
 }
 
 def parse(String description) {
 	def cmd = zwave.parse(description)
-    if(!cmd) return null
-    
-    log.debug cmd
+	if(!cmd) return null
+	
+	log.debug cmd
 	def events = zwaveEvent(cmd)
-    if(!events)
-    	log.warn "No events for command: ${cmd}"
-    return events
+	if(!events)
+		log.warn "No events for command: ${cmd}"
+	return events
 }
 
 def zwaveEvent(physicalgraph.zwave.Command cmd, ep = null) {
@@ -195,11 +195,11 @@ def zwaveEvent(physicalgraph.zwave.Command cmd, ep = null) {
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
-    log.warn "SecurityMessageEncapsulation $cmd"
+	log.warn "SecurityMessageEncapsulation $cmd"
 	def encapsulatedCommand = cmd.encapsulatedCommand(commandClassCapabilities)
-    if (!encapsulatedCommand) return null
-    
-    state.sec = 1
+	if (!encapsulatedCommand) return null
+	
+	state.sec = 1
 	log.debug encapsulatedCommand
 	return zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
 }
@@ -213,37 +213,37 @@ def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap 
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.thermostatsetpointv2.ThermostatSetpointReport cmd, ep = null) {
-    if(!constants.heatingModes.contains(cmd.setpointType as Integer)) return null
+	if(!constants.heatingModes.contains(cmd.setpointType as Integer)) return null
 	if(!cmd.scaledValue) return null
 
 	return createEvent(
-    	name: "heatingSetpoint",
-        value: toClientTemperature(cmd.scaledValue, constants.scale[cmd.scale as Integer]),
-        unit: getTemperatureScale(), 
-       	displayed: false
+		name: "heatingSetpoint",
+		value: toClientTemperature(cmd.scaledValue, constants.scale[cmd.scale as Integer]),
+		unit: getTemperatureScale(), 
+	   	displayed: false
 	)
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.sensormultilevelv5.SensorMultilevelReport cmd, ep = null) {
-    if(cmd.sensorType != 1) return null
+	if(cmd.sensorType != 1) return null
 	if(!cmd.scaledSensorValue) return null
 
-    return createEvent(
-    	name: "temperature",
-        value: toClientTemperature(cmd.scaledSensorValue, constants.scale[cmd.scale as Integer]),
-        unit: getTemperatureScale()
+	return createEvent(
+		name: "temperature",
+		value: toClientTemperature(cmd.scaledSensorValue, constants.scale[cmd.scale as Integer]),
+		unit: getTemperatureScale()
 	)
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.thermostatmodev2.ThermostatModeReport cmd, ep = null) {
 	def mode = operatingModes[cmd.mode]
-    if(!mode) return null
+	if(!mode) return null
 
 	return createEvent([
-        name: "thermostatMode", 
-        value: mode,
-        data: [supportedThermostatModes: supportedModeNames]
-    ])
+		name: "thermostatMode", 
+		value: mode,
+		data: [supportedThermostatModes: supportedModeNames]
+	])
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep = null) {
@@ -256,11 +256,11 @@ def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd, ep = null) 
 
 def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd, ep = null) {
 	def param = configParams[cmd.parameterNumber as Integer]
-    if(!param) return null
-    
-    def desc = "config param ${cmd.parameterNumber}:${param.name} = ${cmd.scaledConfigurationValue} ${param.unit}"
-    log.info desc
-    return createEvent(descriptionText: desc, displayed: true)
+	if(!param) return null
+	
+	def desc = "config param ${cmd.parameterNumber}:${param.name} = ${cmd.scaledConfigurationValue} ${param.unit}"
+	log.info desc
+	return createEvent(descriptionText: desc, displayed: true)
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd, ep = null) {
@@ -327,11 +327,11 @@ def setHeatingSetpoint(setpoint) {
 	sendHubCommand(
 		zwave.thermostatSetpointV2.thermostatSetpointSet(
 			setpointType: type, 
-            scaledValue: limitedValue,
+			scaledValue: limitedValue,
 			scale: currentSetpoint.unit
 	))
-    
-    refresh()
+	
+	refresh()
 }
 
 def switchMode() {
@@ -355,8 +355,8 @@ def setThermostatMode(mode) {
 	sendHubCommand([
 		zwave.thermostatModeV2.thermostatModeSet(mode: setpointType)
 	])
-    
-    refresh()
+	
+	refresh()
 }
 
 def auto(){  }
@@ -374,19 +374,19 @@ private getCurrentTemperature() {
 
 private toClientTemperature(temp, scale) {
 	if (!temp || !scale) return 0
-    
-    def scaledTemp = convertTemperatureIfNeeded(
-    	temp.toBigDecimal(), scale
+	
+	def scaledTemp = convertTemperatureIfNeeded(
+		temp.toBigDecimal(), scale
 	).toDouble()
 
-    return getTemperatureScale() == "F" 
-        ? scaledTemp.round(0).toInteger() 
-        : (Math.round(scaledTemp * 2)) / 2
+	return getTemperatureScale() == "F" 
+		? scaledTemp.round(0).toInteger() 
+		: (Math.round(scaledTemp * 2)) / 2
 }
 
 private toChannel(channel, cmd, bitMask = false) {
 	return zwave.multiChannelV3.multiChannelCmdEncap(
-    	bitAddress: bitMask, 
-        destinationEndPoint: channel
+		bitAddress: bitMask, 
+		destinationEndPoint: channel
 	).encapsulate(cmd)
 }
