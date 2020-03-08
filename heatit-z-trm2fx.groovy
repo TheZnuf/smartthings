6490f8fdc4bd7eab101d783a1303fe4f4ef1eff7
Magnus Solvåg <msolvaag@gmail.com>
2020-03-08 23:30:06 +0100

    Update heatit-z-trm2fx.groovy

--- heatit-z-trm2fx.groovy
+++ heatit-z-trm2fx.groovy
@@ -1,8 +1,19 @@
 /**
- *  Copyright 2020 Magnus Solvåg
+ *  HeatIt Z-TRM2fx
+ *
+ *  Copyright 2020 Magnus
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
  */
 metadata {
-	definition (name: "Heatit Z-TRM2fx", namespace: "ms", author: "Magnus Solvåg", ocfDeviceType: "oic.d.thermostat", cstHandler: true) {
+	definition (name: "Heatit Z-TRM2fx", namespace: "ms", author: "Magnus", ocfDeviceType: "oic.d.thermostat", cstHandler: true) {
 		capability "Actuator"
 		capability "Temperature Measurement"
 		capability "Thermostat Mode"
